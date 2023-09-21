e87e08091a1bc39f448cdda6de5b0cdb97926f32
Almir Vuk <almir.vuk@outlook.com>
2023-09-21 20:53:49 +0200

    Class02 code added.

---
+++ ControllersSample/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using ControllersSample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersSample.Controllers
+{
+    [Route("Ucenici")]
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            // return View("StudentIndex");
+            // return View("List");
+
+           // return RedirectToAction("List");
+           return RedirectToAction("Privacy", "Home");
+        }
+
+        [HttpGet("Lista")]
+        public IActionResult List() 
+        {
+            // This is complex logic which pulls the data from database
+            // this has 1000 lines of code
+
+            ViewResult view = View();
+
+            return view;
+        }
+
+        // [HttpGet("Detalji/{studentId}")] this will be used in the way Ucenici/Detalji/1
+        [HttpGet("Detalji")]
+        public IActionResult Details(int studentId) 
+        {
+            if (studentId == 1)
+            {
+                Student student = new Student()
+                {
+                    Age = 27,
+                    FirstName = "John",
+                    Id = 1,
+                    LastName = "Doe"
+                };
+
+                return new JsonResult(student);
+            }
+            
+            return new EmptyResult();
+        }
+
+        [HttpPost("Kreiraj")]
+        public IActionResult Create([FromBody]Student student) 
+        {
+            return View();
+        }
+
+        [HttpPut("Azuriraj")]
+        public IActionResult Update(Student student)
+        {
+            return View();
+        }
+
+        [HttpDelete("Izbrisi")]
+        public IActionResult Delete(int studentId)
+        {
+            return View();
+        }
+    }
+}
---
+++ ControllersSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControllersSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
