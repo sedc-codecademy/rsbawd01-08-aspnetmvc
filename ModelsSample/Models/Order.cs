8e8fef11a1dad027cb72cc66ceb5ea8597dc874e
Almir Vuk <almir.vuk@outlook.com>
2023-09-23 11:37:11 +0200

    Class03 code added.

---
+++ ModelsSample/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelsSample.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Pizza { get; set; }
+        public double Price { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ ModelsSample/Models/UserOrderDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelsSample.Models
+{
+    public class UserOrderDetailsViewModel
+    {
+        public string Pizza { get; set; }
+        public double Price { get; set; }
+        public string UserInfo { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
