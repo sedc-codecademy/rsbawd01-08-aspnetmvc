42f176b95c1036ba69005593eb3d4af266c1adfe
Almir Vuk <almir.vuk@outlook.com>
2023-09-28 20:58:13 +0200

    Class05 code added.

---
+++ Class05/ViewHelpersSample/Enums/PizzaSize.cs
@@ -0,0 +1,10 @@
+﻿namespace ViewHelpersSample.Enums
+{
+    public enum PizzaSize
+    {
+        Small = 0,
+        Medium = 1,
+        Large = 2,
+        Family = 3,
+    }
+}
---
+++ ViewHelpersSample/Models/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ViewHelpersSample.Enums;
+
+namespace ViewHelpersSample.Models.ViewModels
+{
+    public class OrderDetailsViewModel
+    {
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime Date { get; set; }
+
+        public PizzaSize Size { get; set; }
+    }
+}
