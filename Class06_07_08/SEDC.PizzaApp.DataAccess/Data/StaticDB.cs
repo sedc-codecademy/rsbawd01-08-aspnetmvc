b0f38b82569d1dd46493fdabf613f8ad475bc7b3
Almir Vuk <almir.vuk@outlook.com>
2023-10-05 18:55:05 +0200

    Class 07 code added.

--- Class06_07_08/SEDC.PizzaApp.DataAccess/Data/StaticDB.cs
+++ Class06_07_08/SEDC.PizzaApp.DataAccess/Data/StaticDB.cs
@@ -4,6 +4,8 @@ namespace SEDC.PizzaApp.DataAccess.Data
 {
     public static class StaticDB
     {
+        public static int OrderId = 1;
+
         public static List<Pizza> Pizzas { get; set; }
         public static List<Order> Orders { get; set; }
         public static List<PizzaOrder> PizzaOrders { get; set; }
--- Class06_07_08/SEDC.PizzaApp.DataAccess/Repositories/OrderRepository.cs
+++ Class06_07_08/SEDC.PizzaApp.DataAccess/Repositories/OrderRepository.cs
@@ -1,32 +1,20 @@
 ﻿using SEDC.PizzaApp.DataAccess.Data;
 using SEDC.PizzaApp.DataAccess.Domain.Models;
 using SEDC.PizzaApp.DataAccess.Repositories.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SEDC.PizzaApp.DataAccess.Repositories
 {
     public class OrderRepository : IRepository<Order>
     {
-        public void Delete(Order entity)
-        {
-            throw new NotImplementedException();
-        }
-
         public List<Order> GetAll()
         {
-            throw new NotImplementedException();
+            return StaticDB.Orders;
         }
 
         public Order GetById(int id)
         {
-            // get one item from static db, Orders list, where id == id from the list
-            List<Order> orders = StaticDB.Orders;
-
-            Order order = orders.Where(o => o.Id == id)
+            Order order = StaticDB.Orders
+                .Where(o => o.Id == id)
                 .FirstOrDefault();
 
             return order;
@@ -34,12 +22,34 @@ namespace SEDC.PizzaApp.DataAccess.Repositories
 
         public int Insert(Order entity)
         {
-            throw new NotImplementedException();
+            int newId = StaticDB.OrderId++;
+
+            StaticDB.Orders.Add(entity);
+
+            return newId;
         }
 
         public void Update(Order entity)
         {
-            throw new NotImplementedException();
+            Order order = StaticDB.Orders
+                .Where(o => o.Id == entity.Id)
+                .FirstOrDefault();
+
+            int index = StaticDB.Orders.IndexOf(order);
+
+            StaticDB.Orders[index] = entity;
+        }
+
+        public void Delete(Order entity)
+        {
+            Order order = StaticDB.Orders
+                .Where(o => o.Id == entity.Id)
+                .FirstOrDefault();
+
+            if (order == null)
+                throw new Exception($"There is no record with {entity.Id}");
+        
+            StaticDB.Orders.Remove(order);
         }
     }
 }
