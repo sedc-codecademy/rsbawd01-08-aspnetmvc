190919f68ab5ad44cb28fa13006390137263b90a
Almir Vuk <almir.vuk@outlook.com>
2023-10-10 19:10:19 +0200

    Class09 code added.

---
+++ Class06_07_08/SEDC.PizzaApp.DataAccess/Data/PizzaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEDC.PizzaApp.DataAccess.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.DataAccess.Data
+{
+    public class PizzaDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        public PizzaDbContext(DbContextOptions options) : base(options) { }
+    }
+}
